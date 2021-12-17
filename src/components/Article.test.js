b8fb49c7fc6f86b1025b120249281773b24028ec
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2021-12-17 16:24:26 -0600

    completed article test

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,18 +5,53 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
+import { render, screen, waitFor } from '@testing-library/react';
+
+const testArticle = [
+  {
+    id: 'asdf',
+    headline: 'title',
+    author: '',
+    createdOn: '123',
+    summary: 'This is a summary',
+    body: ''
+
+  }
+]
 
 test('renders component without errors', ()=> {
+  render(<Article article={testArticle}/>)
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', ()=> {
+  render(<Article article={testArticle}/>);
 
-// test('renders "Associated Press" when no author is given', ()=> {
-// });
+  const headline = screen.getByTestId(/headline/i);
+  const  author = screen.getByTestId(/author/i);
 
-// test('executes handleDelete when the delete button is pressed', ()=> {
-// });
+  expect(headline).toBeInTheDocument();
+  expect(author).toBeInTheDocument();
+});
+
+test('renders "Associated Press" when no author is given', ()=> {
+  render(<Article article={testArticle}/>);
+
+  const author = screen.getByTestId(/author/i);
+
+  expect(author.textContent).toContain('Associated Press');
+});
+
+test('executes handleDelete when the delete button is pressed', async ()=> {
+  const mock = jest.fn()
+
+  render(<Article article={testArticle} handleDelete={mock}/>);
+
+  const deleteBtn = screen.getByTestId(/deleteButton/i);
+  userEvent.click(deleteBtn);
+
+  await waitFor(() => expect(mock).toBeCalled())
+
+});
 
 //Task List:
 //1. Complete all above tests. Create test article data when needed.
